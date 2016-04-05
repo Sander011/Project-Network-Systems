548d1494a403ea83371d6b198a2a40db792a206e
Ties Bolding <tiesbolding@outlook.com>
2016-04-05 10:57:56 +0200

    Transmission: add simple receiver

--- ChatAdHoc/src/Transmission.java
+++ ChatAdHoc/src/Transmission.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.MulticastSocket;
 import java.util.Observable;
 
@@ -5,25 +7,38 @@ import java.util.Observable;
  * Created by sander on 5-4-16.
  */
 public class Transmission extends Observable {
+    private class Receiver extends Thread {
+        private MulticastSocket socket;
+
+        Receiver(MulticastSocket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+                try {
+                    socket.receive(packet);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     private MulticastSocket socket;
     private ForwardingTable table;
 
     public Transmission(MulticastSocket socket, ForwardingTable table) {
         this.socket = socket;
         this.table = table;
-    }
 
-
-    private void parseMessage() {
-
+        new Receiver(socket).start();
     }
 
     public void send(String msg) {
 
     }
-
-
-
-
 }
 
