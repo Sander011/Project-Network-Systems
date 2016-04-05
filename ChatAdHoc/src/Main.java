314c90748b914f5797610ac6c79f4277bea7935a
Sander011 <sander_meinderts@hotmail.com>
2016-04-05 09:35:40 +0200

    Main method

--- ChatAdHoc/src/Main.java
+++ ChatAdHoc/src/Main.java
@@ -2,4 +2,14 @@
  * Created by sander on 5-4-16.
  */
 public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+    }
+
+    public Main () {
+
+    }
+
+
+
 }
