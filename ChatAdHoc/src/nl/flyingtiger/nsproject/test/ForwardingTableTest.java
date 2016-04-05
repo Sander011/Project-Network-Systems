55cdf88452ce5ca9e836fff43f3077896288164c
Sander011 <sander_meinderts@hotmail.com>
2016-04-05 12:00:04 +0200

    Fixed Byte[] -> string

--- ChatAdHoc/src/nl/flyingtiger/nsproject/test/ForwardingTableTest.java
+++ ChatAdHoc/src/nl/flyingtiger/nsproject/test/ForwardingTableTest.java
@@ -6,8 +6,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 
+import java.io.UnsupportedEncodingException;
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,7 +30,7 @@ public class ForwardingTableTest {
 
 
     @Test
-    public void testInitialTableEntry() throws UnknownHostException {
+    public void testInitialTableEntry() throws UnknownHostException, UnsupportedEncodingException {
         HashMap<String, TableEntry> forwardTable = table.getTable();
         for (Map.Entry<String, TableEntry> tableEntry : forwardTable.entrySet()) {
             assertTrue(tableEntry.getKey().equals(Inet4Address.getLocalHost().toString()));
@@ -35,9 +38,18 @@ public class ForwardingTableTest {
             assertTrue(tableEntry.getValue().getHopCount() == 0);
             assertTrue(tableEntry.getValue().getSeqNumber() == 0);
         }
+
+        char[] buffer = table.toString().toCharArray();
+        byte[] b = new byte[buffer.length << 1];
+        CharBuffer cBuffer = ByteBuffer.wrap(b).asCharBuffer();
+        for(int i = 0; i < buffer.length; i++)
+        cBuffer.put(buffer[i]);
+
+        cBuffer = ByteBuffer.wrap(b).asCharBuffer();
+
         System.out.println(table.toString());
-        System.out.println(table.toByteArray());
-        System.out.println(table.toByteArray().toString());
+        System.out.println(b);
+        System.out.println(cBuffer.toString());
     }
 
 }
