408f31f0533703a91efe2ac4872d042a4db10531
Jasper Sustronk <j.j.sustronk11@hotmail.com>
2016-04-05 10:56:05 +0200

    Created Header and TypeOfPacket

---
+++ ChatAdHoc/src/Header.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Jasper on 5-4-2016.
+ */
+public class Header {
+
+
+}
---
+++ ChatAdHoc/src/TypeOfPacket.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Jasper on 5-4-2016.
+ */
+public enum TypeOfPacket {
+
+    FORWARDINGTABLE, TEXT, FILE;
+
+}
